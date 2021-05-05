ea9da901ebf40d74c0f9388a7520b065114ee117
rainbowCat <jazziblue@gmail.com>
2021-05-04 18:18:20 -0700

    move code to python files

--- test_submission.py
+++ test_submission.py
@@ -1,16 +1,27 @@
-import json, sys
+import json
+import sys
+
 
 def eval(text):
-	# This is where you call your model to get the number of stars output
-	return 1.0
+    # This is where you call your model to get the number of stars output
+    return 1.0
+
 
 if len(sys.argv) > 1:
-	validation_file = sys.argv[1]
-	with open("output.jsonl", "w") as fw:
-		with open(validation_file, "r") as fr:
-			for line in fr:
-				review = json.loads(line)
-				fw.write(json.dumps({"review_id": review['review_id'], "predicted_stars": eval(review['text'])})+"\n")
-	print("Output prediction file written")
+    validation_file = sys.argv[1]
+    with open("output.jsonl", "w") as fw:
+        with open(validation_file, "r") as fr:
+            for line in fr:
+                review = json.loads(line)
+                fw.write(
+                    json.dumps(
+                        {
+                            "review_id": review["review_id"],
+                            "predicted_stars": eval(review["text"]),
+                        }
+                    )
+                    + "\n"
+                )
+    print("Output prediction file written")
 else:
-	print("No validation file given")
+    print("No validation file given")
