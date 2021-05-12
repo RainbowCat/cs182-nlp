370a1d6c1519877b75f28e425f7d573f4b6f5405
Alok Singh <alokbeniwal@gmail.com>
2021-05-11 18:18:01 -0700

    parametrize main

--- main.py
+++ main.py
@@ -9,34 +9,40 @@ import data
 import models
 
 
-def main():
-    for use_bert, use_vader in itertools.product([True, False], repeat=2):
-        print(f"{use_bert, use_vader=}")
+def main(
+    use_bert: bool,
+    use_vader: bool,
+    batch_size: int = 240,
+    epochs: int = 10,
+    max_len: int = 128,
+    max_len_vader: int = 128,
+) -> None:
+    args = Namespace(
+        batch_size=batch_size,
+        epochs=epochs,
+        max_len=max_len,
+        max_len_vader=max_len_vader,
+        use_bert=use_bert,
+        use_vader=use_vader,
+    )
 
-        args = Namespace(
-            batch_size=240,
-            epochs=10,
-            max_len=128,
-            max_len_vader=128,
-            use_bert=use_bert,
-            use_vader=use_vader,
-        )
-
-        model = models.LanguageModel(args)
-        datamodule = data.YelpDataModule(args)
-        trainer = pl.Trainer(
-            gpus=torch.cuda.device_count(),
-            max_epochs=args.epochs,
-            # overfit_batches=1,
-            # track_grad_norm=2,
-            weights_summary="full",
-            progress_bar_refresh_rate=1,
-            check_val_every_n_epoch=1,
-            callbacks=[ModelCheckpoint(monitor="val_loss", every_n_train_steps=1_000)],
-        )
-        trainer.fit(model, datamodule=datamodule)
-        trainer.validate(model, datamodule=datamodule)
+    model = models.LanguageModel(args)
+    datamodule = data.YelpDataModule(args)
+    trainer = pl.Trainer(
+        gpus=torch.cuda.device_count(),
+        max_epochs=args.epochs,
+        # overfit_batches=1,
+        # track_grad_norm=2,
+        weights_summary="full",
+        progress_bar_refresh_rate=1,
+        check_val_every_n_epoch=1,
+        callbacks=[ModelCheckpoint(monitor="val_loss", every_n_train_steps=1_000)],
+    )
+    trainer.fit(model, datamodule=datamodule)
+    trainer.validate(model, datamodule=datamodule)
 
 
 if __name__ == "__main__":
-    main()
+    for use_bert, use_vader in itertools.product([True, False], repeat=2):
+        print(f"{use_bert, use_vader=}")
+        main(use_bert, use_vader)
